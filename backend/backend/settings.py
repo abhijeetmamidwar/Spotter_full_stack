5aef3ff6d06fbb0ff278c1487711beb048390935
Abhijeet Mamidwar <abhijeetsm2001@gmail.com>
2025-12-02 20:06:26 +0530

    All vercel endpoints allowed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     # Added below middleware
     "corsheaders.middleware.CorsMiddleware",
-    
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,6 +63,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    r"^https:\/\/.*\.vercel\.app$", # All vercel endpoints
     'https://spotter-full-stack-git-main-abhijeets-projects-d2f4c120.vercel.app',
     "http://localhost:5173",
 ]
